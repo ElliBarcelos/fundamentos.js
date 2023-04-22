2d9069f052a91b93aed6c59a169f57023c86e5af
ElliBarcelos <ellybarcelos40@gmail.com>
2023-04-22 18:05:05 -0300

    operadores e funcoes

---
+++ expressoes-funcao.js
@@ -0,0 +1,24 @@
+// //declaraçao de função
+
+// function minhaFuncao (param) {
+//     //bloco de codigo
+// }
+
+// minhaFuncao("param")
+
+// //expressao de função
+
+// const soma = function(num1, num2) {return num1 + num2}
+// console.log(soma(1, 1))
+
+// //diferença principal?????
+// //função e var são "listadas" no topo
+
+console.log (apresentar())
+
+function apresentar() {
+    return "Olá";
+}
+
+console.log(soma(1, 1))
+const soma = function(num1, num2) {return num1 + num2}
---
+++ template-String.js
@@ -0,0 +1,10 @@
+const nome = "Elli";
+const idade = 2023-1983;
+const cidadeDeNascimento = "Rio de Janeiro"
+
+// const aprensentacao = "meu nome é " + nome +", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const aprensentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(aprensentacao)
+
